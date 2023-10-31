561694857a3f72f03dff421c5f3eb5d68d8f75f9
Gabriel Sastre <gabisas@icloud.com>
2023-10-31 14:36:18 +0100

    logica de comparacion de cartas y contador de intentos

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
+// const cards = document.querySelectorAll(".card");
+
+// const reveal = (e) => {
+//   const currentCard = e.currentTarget;
+//   currentCard.classList.add("flipped");
+
+//   setTimeout(() => {
+//     currentCard.classList.remove("flipped");
+//   }, 1000);
+// };
+
+// for (const card of cards) {
+//   card.addEventListener("click", reveal);
+// }
+
 const cards = document.querySelectorAll(".card");
+let stopper = 0;
+let cardUpp = 0;
+const cardUpped = [];
 
+///////
+// const reveal = (e) => {
+
+//   const currentCard = e.currentTarget;
+//   currentCard.classList.add("flipped");
+
+//   setTimeout(() => {
+//     currentCard.classList.remove("flipped");
+//   }, 1000);
+// };
+///////
+let bloqueo = 0;
 const reveal = (e) => {
-  const currentCard = e.currentTarget;
-  currentCard.classList.add("flipped");
-  console.log(cards)
+  console.log("esta es la carta que he levantado", e);
+  if (!cardUpped.includes(e.currentTarget)) {
+    if (stopper < 2 && cardUpp !== e.currentTarget && bloqueo === 0) {
+      stopper++;
+      const currentCard = e.currentTarget;
 
-  // setTimeout(() => {
-  //   currentCard.classList.remove("flipped");
-  // }, 1000);
+      currentCard.classList.add("flipped");
+      console.log("informacion carta levantada", currentCard);
+
+      // setTimeout(() => {
+      //   currentCard.classList.remove("flipped");
+      // }, 100000);
+
+      if (stopper === 2) {
+        bloqueo = 1;
+        console.log("has levantado dos cartas iguales");
+        stopper = 0;
+
+        // if(cardUpp === e.currentTarget)
+        cardUpped.push(cardUpp);
+
+        cardUpped.push(e.currentTarget);
+        console.log("cartas levantadas", cardUpped);
+        const cardtoRemove = cardUpp;
+
+        setTimeout(() => {
+          currentCard.classList.add("erased");
+          cardtoRemove.classList.add("erased");
+          currentCard.classList.remove("flipped");
+          cardtoRemove.classList.remove("flipped");
+          bloqueo = 0;
+          // currentCard.classList.remove("flipped");
+        }, 3000);
+        // } else {
+        //   console.log("No son iguales");
+        //   setTimeout(() => {
+        //     currentCard.classList.remove("flipped");
+        //     cardUpp.classList.remove("flipped");
+        //   }, 3000);
+        //   stopper = 0;
+        // }
+      }
+      cardUpp = e.currentTarget;
+    }
+  }
 };
 
 for (const card of cards) {
